0b5312f67c583b9fee61f68cd885e6dbdeb89838
Your Name <you@example.com>
2021-05-28 17:43:23 +0900

    mysql 연도 step1

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask , render_template 
+from flask import Flask , render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/',methods=['GET','POST'])
-
+@app.route('/', methods=['GET' , 'POST'])
 def hello_world():
-    return render_template('home.html' , name="김승현")
+    return render_template('home.html' , name="김태경")
+
+@app.route('/about', methods=['GET' , 'POST'])
+def about():
+    return render_template("about.html")
+
+@app.route('/articles', methods=['GET' , 'POST'])
+def articles():
+    articles = Articles()
+    return render_template("articles.html" , articles = articles )
+
+@app.route('/article/<id>',methods=['GET','POST'])
+def article(id):
+    articles = Articles()
+    
+    print(len(articles))
+    if len(articles) >= int(id):
+        article= articles[int(id)-1]
+        return render_template('article.html', article= articles)
+
+    else:
+        return render_template('article.html', article= 'NO DATA')
+
+@app.route('/add_article',methods=['GET','POST'])
+def add_article():
+    return render_template("add_article.html" )
+      
 
 if __name__ == '__main__':
     app.run(port=5000)
-
-@app.route('/about',methods=['GET','POST'])
-
-
-def about():
-    return render_template('about.html')
